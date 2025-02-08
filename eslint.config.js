e64a3c3c8db49fefa9bfbe868092d819a45d6b13
Franklin Shera <fshera96@gmail.com>
2025-02-08 18:40:32 +0300

    setup linting and formatting

---
+++ eslint.config.js
@@ -0,0 +1,71 @@
+import astroEslintParser from "astro-eslint-parser";
+import tseslint from "typescript-eslint";
+import typescriptEslintParser from "@typescript-eslint/parser";
+import astroPlugin from "eslint-plugin-astro";
+import globals from "globals";
+
+export default tseslint.config(
+  {
+    ignores: ["dist"],
+  },
+  ...astroPlugin.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,ts}"],
+    languageOptions: {
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+      parser: typescriptEslintParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        tsconfigRootDir: process.cwd(),
+      },
+    },
+  },
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroEslintParser,
+      parserOptions: {
+        parser: typescriptEslintParser,
+        extraFileExtensions: [".astro"],
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
+  {
+    rules: {
+      "padding-line-between-statements": [
+        "warn",
+        { blankLine: "always", prev: "*", next: ["return", "export"] },
+        {
+          blankLine: "always",
+          prev: ["const", "let", "var", "block-like", "export"],
+          next: "*",
+        },
+        {
+          blankLine: "always",
+          prev: ["const", "let", "var", "block-like", "export"],
+          next: ["const", "let", "var", "block-like", "export"],
+        },
+      ],
+      "no-console": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          args: "all",
+          ignoreRestSiblings: false,
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+);
