206a49a637b2ab6a60a3280f09f722499f060e55
Franklin Shera <fshera96@gmail.com>
2025-02-12 10:49:44 +0300

    allow any in *.astro

--- eslint.config.js
+++ eslint.config.js
@@ -36,6 +36,7 @@ export default tseslint.config(
     },
     rules: {
       "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
   {
