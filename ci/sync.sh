40974ad53f0f1d3959ce6e25666433d2f574d5ee
Stephen Whitfield <swhitf@deqo.io>
2025-05-14 12:03:13 +0100

    Added drone

--- ci/sync.sh
+++ ci/sync.sh
@@ -13,4 +13,8 @@ ssh $REMOTE_HOST "mkdir -p $REMOTE_DIR"
 echo "Syncing local dist folder to remote server using rsync..."
 rsync -avz --delete ./dist/ $REMOTE_HOST:$REMOTE_DIR/
 
+echo "Busting caches"
+curl -H "AccessKey:2c850977-17b0-436e-96fb-79cad7379c326fba050a-e10f-43ed-92f3-deb6f8d7d84e" --data "" https://bunnycdn.com/api/pullzone/2393713/purgeCache
+curl https://metroretro.io?flush=1 -o /dev/null
+
 echo "Process completed successfully."
